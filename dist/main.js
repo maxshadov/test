873e56634822c5fb3e4712935021123fbbc1cba0
Dmitry Tkachenko <dmitry.tkachenko@onsolve.com>
2022-02-10 17:09:11 +0200

    Backup test

--- dist/main.js
+++ dist/main.js
@@ -1,41 +1,14 @@
 "use strict";
 const Dropbox = require('dropbox').Dropbox;
-const fs = require('fs');
 const axios = require('axios');
 const fetch2 = require('node-fetch');
 const core = require('@actions/core');
-const github = require('@actions/github');
-const glob = require('glob');
+
 const accessToken = core.getInput('DROPBOX_ACCESS_TOKEN');
-const globSource = core.getInput('GLOB');
 const dropboxPathPrefix = core.getInput('DROPBOX_DESTINATION_PATH_PREFIX');
 const isDebug = core.getInput('DEBUG');
 const key = core.getInput('GIT_KEY');
 const dropbox = new Dropbox({accessToken: accessToken, fetch: fetch2});
-
-// const uploadMuhFile = (filePath) => {
-//     const file = fs.readFileSync(filePath);
-//     const destinationPath = `${dropboxPathPrefix}/${filePath}`;
-//     if (isDebug)
-//         console.log('uploaded file to Dropbox at: ', destinationPath);
-//     return dropbox
-//         .filesUpload({ path: destinationPath, contents: file })
-//         .then(response => {
-//         if (isDebug)
-//             console.log(response);
-//         return response;
-//     })
-//         .catch(error => {
-//         if (isDebug)
-//             console.error(error);
-//         return error;
-//     });
-// }
-const blobToFile = (theBlob, fileName) => {
-    theBlob.lastModifiedDate = new Date();
-    theBlob.name = fileName;
-    return theBlob;
-}
 
 axios
     .get('https://api.github.com/repos/maxshadov/test/actions/artifacts', {
@@ -44,6 +17,7 @@ axios
         }
     })
     .then(res => {
+        console.log(res.data.artifacts[0].created_at);
         axios({
             url: res.data.artifacts[0].archive_download_url,
             method: 'GET',
@@ -52,14 +26,11 @@ axios
                 'Authorization': `Bearer ${key}`
             }
         }).then(blob => {
-            const date = new Date();
-            const suff = `${date.getDate()}-${date.getMonth()}`
-            const destinationPath = `/Sanity${dropboxPathPrefix}-${suff}.zip`;
+            const destinationPath = `/Sanity/${dropboxPathPrefix}/backup-${res.data.artifacts[0].created_at}.zip`;
             if (isDebug)
                 console.log('uploaded file to Dropbox at: ', destinationPath);
             return dropbox
-                .filesUpload({path: destinationPath, contents: blob.data})
-                .then(response => {
+                .filesUpload({path: destinationPath, contents: blob.data}).then(response => {
                     if (isDebug)
                         console.log(response);
                     return response;
@@ -73,15 +44,3 @@ axios
 
     })
     .catch(() => console.log('Some error'));
-
-// glob(globSource, {}, (err, files) => {
-//     if (err)
-//         core.setFailed('Error: glob failed', err);
-//     Promise.all(files.map(uploadMuhFile))
-//         .then(all => {
-//         console.log('all files uploaded', all);
-//     })
-//         .catch(err => {
-//         console.error('error', err);
-//     });
-// });
