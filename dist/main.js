274960eafd93f6b78ed943a333e317d849cd28a3
Dmitry Tkachenko <dmitry.tkachenko@onsolve.com>
2022-02-10 13:52:33 +0200

    Backup test

--- dist/main.js
+++ dist/main.js
@@ -1,15 +1,14 @@
 "use strict";
-// const Dropbox = require('dropbox').Dropbox;
-// import fs from 'fs';
-// const fetch2 = require('node-fetch');
-// const core = require('@actions/core');
-// const github = require('@actions/github');
-// const glob = require('glob');
-import core from '@actions/core';
-// const accessToken = core.getInput('DROPBOX_ACCESS_TOKEN');
-// const globSource = core.getInput('GLOB');
-// const dropboxPathPrefix = core.getInput('DROPBOX_DESTINATION_PATH_PREFIX');
-// const isDebug = core.getInput('DEBUG');
+const Dropbox = require('dropbox').Dropbox;
+const fs = require('fs');
+const fetch2 = require('node-fetch');
+const core = require('@actions/core');
+const github = require('@actions/github');
+const glob = require('glob');
+const accessToken = core.getInput('DROPBOX_ACCESS_TOKEN');
+const globSource = core.getInput('GLOB');
+const dropboxPathPrefix = core.getInput('DROPBOX_DESTINATION_PATH_PREFIX');
+const isDebug = core.getInput('DEBUG');
 const key = core.getInput('GIT_KEY');
 // const dropbox = new Dropbox({accessToken: accessToken, fetch: fetch2});
 
