5c5dcad9ac6915776f995889c7c7fd44ad9bd8f0
Dmitry Tkachenko <dmitry.tkachenko@onsolve.com>
2022-02-10 15:25:48 +0200

    Backup test

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,5 @@
 "use strict";
 const Dropbox = require('dropbox').Dropbox;
-const dropboxV2Api = require('dropbox-v2-api');
 const fs = require('fs');
 const axios = require('axios');
 const fetch2 = require('node-fetch');
@@ -12,10 +11,8 @@ const globSource = core.getInput('GLOB');
 const dropboxPathPrefix = core.getInput('DROPBOX_DESTINATION_PATH_PREFIX');
 const isDebug = core.getInput('DEBUG');
 const key = core.getInput('GIT_KEY');
-// const dropbox = new Dropbox({accessToken: accessToken, fetch: fetch2});
-const dropbox = dropboxV2Api.authenticate({
-    token: accessToken
-});
+const dropbox = new Dropbox({accessToken: accessToken, fetch: fetch2});
+
 // const uploadMuhFile = (filePath) => {
 //     const file = fs.readFileSync(filePath);
 //     const destinationPath = `${dropboxPathPrefix}/${filePath}`;
@@ -41,6 +38,7 @@ axios
         }
     })
     .then(res => {
+        console.log(res.data.artifacts[0].archive_download_url)
         let file;
         axios({
             url: res.data.artifacts[0].archive_download_url,
@@ -51,34 +49,23 @@ axios
             }
         }).then((response) => {
             file = response.data.pipe(fs.createWriteStream("back.zip"));
-            const stream = dropbox({
-                resource: 'files/upload_session/start',
-                parameters: {
-                    'close': false
-                }
-            }, (err, result, response) => {
-                console.log(err);
-            });
-
-            fs.createReadStream(file).pipe(stream);
-        })
-        // .then(() => {
-        //     const destinationPath = `${dropboxPathPrefix}.zip`;
-        //     if (isDebug)
-        //         console.log('uploaded file to Dropbox at: ', destinationPath);
-        //     return dropbox
-        //         .filesUpload({path: destinationPath, contents: file})
-        //         .then(response => {
-        //             if (isDebug)
-        //                 console.log(response);
-        //             return response;
-        //         })
-        //         .catch(error => {
-        //             if (isDebug)
-        //                 console.error(error);
-        //             return error;
-        //         });
-        // });
+        }).then(() => {
+            const destinationPath = `${dropboxPathPrefix}.zip`;
+            if (isDebug)
+                console.log('uploaded file to Dropbox at: ', destinationPath);
+            return dropbox
+                .filesUpload({path: destinationPath, contents: file})
+                .then(response => {
+                    if (isDebug)
+                        console.log(response);
+                    return response;
+                })
+                .catch(error => {
+                    if (isDebug)
+                        console.error(error);
+                    return error;
+                });
+        });
 
     })
     .catch(() => console.log('Some error'));
